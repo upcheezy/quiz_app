589f58b06a469ceb8177626be2312033e30e44cc
Matthew Upchurch <upcheezy@gmail.com>
2020-01-30 15:19:21 -0500

    added data structure to store.js

---
+++ store.js
@@ -0,0 +1,102 @@
+const STORE = [
+    // one
+    {
+        question: 'Which continent includes the Jutland and Iberian Peninsulas?',
+        options: [
+            'Asia',
+            'Europe',
+            'North America',
+            'Australia'
+        ]
+    },
+    // two
+    {
+        question: 'Which continent includes the Jutland and Iberian Peninsulas?',
+        options: [
+            'Asia',
+            'Europe',
+            'North America',
+            'Australia'
+        ]
+    },
+    // three
+    {
+        question: 'Which continent includes the Jutland and Iberian Peninsulas?',
+        options: [
+            'Asia',
+            'Europe',
+            'North America',
+            'Australia'
+        ]
+    },
+    // four
+    {
+        question: 'Which continent includes the Jutland and Iberian Peninsulas?',
+        options: [
+            'Asia',
+            'Europe',
+            'North America',
+            'Australia'
+        ]
+    },
+    // five
+    {
+        question: 'Which continent includes the Jutland and Iberian Peninsulas?',
+        options: [
+            'Asia',
+            'Europe',
+            'North America',
+            'Australia'
+        ]
+    },
+    // six
+    {
+        question: 'Which continent includes the Jutland and Iberian Peninsulas?',
+        options: [
+            'Asia',
+            'Europe',
+            'North America',
+            'Australia'
+        ]
+    },
+    // seven
+    {
+        question: 'Which continent includes the Jutland and Iberian Peninsulas?',
+        options: [
+            'Asia',
+            'Europe',
+            'North America',
+            'Australia'
+        ]
+    },
+    // eight
+    {
+        question: 'Which continent includes the Jutland and Iberian Peninsulas?',
+        options: [
+            'Asia',
+            'Europe',
+            'North America',
+            'Australia'
+        ]
+    },
+    // nine
+    {
+        question: 'Which continent includes the Jutland and Iberian Peninsulas?',
+        options: [
+            'Asia',
+            'Europe',
+            'North America',
+            'Australia'
+        ]
+    },
+    // ten
+    {
+        question: 'Which continent includes the Jutland and Iberian Peninsulas?',
+        options: [
+            'Asia',
+            'Europe',
+            'North America',
+            'Australia'
+        ]
+    }
+]
