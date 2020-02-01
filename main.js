12147ef643f91787659b15e518a162119e6b1d62
Matthew Upchurch <upcheezy@gmail.com>
2020-02-01 15:31:06 -0500

    updated files

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ function getStarted() {
 /*displays the question*/
 function renderAQuestion() {
     let question = STORE[0];
-    console.log(question.question);
+    // console.log(question.question);
     // updateQuestionAndScore();
     const questionHtml = $(`
     <h3>Question 1</h3>
-        <form>
+        <form id='js-questions'>
             <fieldset>
               <legend class="questionText">${question.question}</legend>
               <input type="radio" name="gender" value="option1" checked>${question.options[0]}<br>
@@ -21,11 +21,69 @@ function renderAQuestion() {
                 <input type="radio" name="gender" value="option4">${question.options[3]}
             </fieldset>
         </form>
-        <button type="button" class="startButton button">Next</button>`);
-    console.log(questionHtml);
-  $("quiz").find('inner-quiz-box').html(questionHtml);
-//   updateOptions();
-// $("#next-question").hide();
+        <button type="submit" class="questionButton button">Next</button>`);
+    // console.log(questionHtml);
+  $(".inner-quiz-box").html(questionHtml);
 }
 
-$(getStarted())
+// console.log(STORE[0]);
+
+function addQuestionNumber() {
+    for (let i=0; i < STORE.length; i++) {
+        // console.log(i);
+        STORE[i].currentQuestion = i
+    }
+
+    // console.log(STORE)
+}
+
+// this will handle looping through the store object and adding the next question each time
+function nextQuestion() {
+    // STORE.score = 0;
+    let questionNumber = 1
+    $('.quiz').on('click', '.questionButton', function(event) {
+        // alert('you just clicked the next button')
+        item = STORE[questionNumber];
+        questionNumber += 1;
+        if (questionNumber > 10) {
+            alert("sorry, that's the end of the quiz");
+            getStarted();
+        } else {
+            $(".inner-quiz-box").html(updateQuestion(questionNumber, item));
+        }
+    })
+}
+
+function updateQuestion(questionNumber, item) {
+    return `
+    <h3>Question ${questionNumber}</h3>
+        <form id='js-questions'>
+            <fieldset>
+              <legend class="questionText">${item.question}</legend>
+              <input type="radio" name="gender" value="option1" checked>${item.options[0]}<br>
+                <input type="radio" name="gender" value="option2">${item.options[1]}<br>
+                <input type="radio" name="gender" value="option3">${item.options[2]}<br>
+                <input type="radio" name="gender" value="option4">${item.options[3]}
+            </fieldset>
+        </form>
+        <button type="submit" class="questionButton button">Next</button>`;
+}
+
+function determineCorrectness() {
+    console.log('inside determineCorrectness')
+    $('#js-questions').submit(function(event) {
+        event.preventDefault();
+        let currentQues = STORE.currentQuestion;
+        let selectedOption = $("input[name=options]:checked").val();
+        console.log(currentQues);
+    })
+}
+
+function handleQuizApp() {
+    getStarted();
+    addQuestionNumber();
+    nextQuestion();
+    determineCorrectness();
+}
+
+$(handleQuizApp());
