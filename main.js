b30d8ec2dfb88603a1b6898ebc3c357fca98114f
Matthew Upchurch <upcheezy@gmail.com>
2020-02-05 10:51:21 -0500

    everthing is working except the final checkbox and refresh all checkboxes upon start over

--- main.js
+++ main.js
@@ -4,23 +4,28 @@ function getStarted() {
   })
 }
 
+let questionNumber = 0;
+let score = 0;
 // this will handle looping through the store object and adding the next question each time
 function nextQuestion() {
-  let questionNumber = 0
+  // let questionNumber = 0
   $('.quiz').on('submit', '#js-questions', function (event) {
     event.preventDefault();
 
     let item = STORE[questionNumber];
     let selectedOption = $("input[name=option]:checked").val();
-
-    questionNumber += 1;
-    if (questionNumber > 9) {
-      alert("sorry, that's the end of the quiz");
-      // getStarted();
+    console.log(questionNumber);
+    if (questionNumber > 8) {
+      // need to create a new function for this section to handle the last question
+      // as well as a scoring page
+      console.log('inside')
+      determineFinalCorrectness(selectedOption, item);
+      // questionNumber = 0;
     } else {
-      determineCorrectness(selectedOption, item);
       $(".inner-quiz-box").html(renderAQuestion(questionNumber));
+      determineCorrectness(selectedOption, item, questionNumber);
     }
+    questionNumber += 1;
   })
 }
 
@@ -44,31 +49,35 @@ function renderAQuestion(questionNum) {
   $(".inner-quiz-box").html(questionHtml);
 }
 
-function determineCorrectness(selectedOption, item) {
+function determineCorrectness(selectedOption, item, questionNumber) {
   let currentAns = item.answer;
-  console.log(currentAns);
-  console.log(selectedOption);
-  // if (currentQues === selectedOption) {
-  //   renderFeedback(selectedOption);
-  // } else {
-  //   handleIncorrect();
-  // }
+  // console.log(currentAns);
+  // console.log(selectedOption);
 
   const isCorrect = currentAns === selectedOption ? true : false;
-  console.log(isCorrect);
-  renderFeedback(isCorrect, currentAns);
+  // console.log(isCorrect);
+  renderFeedback(isCorrect, currentAns, questionNumber);
 }
 
-function renderFeedback(isCorrect, currentAns) {
-  // HTML portion of feedback reliant on correctness 
-  const correctnessHTML = isCorrect ? `Inccorrect, correct answer is ${currentAns}` : `Correct!`
-  console.log(correctnessHTML);
+function renderFeedback(isCorrect, currentAns, questionNumber) {
+  // HTML portion of feedback reliant on correctness
+  if (isCorrect) {
+    console.log('inside rederfeedback');
+    $(`li #${questionNumber}`).removeClass('far fa-square').addClass('far fa-plus-square');
+    // console.log(imageCont);
+    correctnessHTML = `Correct!`;
+    score += 1;
+  } else {
+    // alert('esNawt')
+    $(`li #${questionNumber}`).removeClass('far fa-square').addClass('far fa-minus-square');
+    correctnessHTML = `Inccorrect, correct answer is ${currentAns}`
+  }
 
   // incorporate that into the general HTML feedback 
   const feedbackHTML = `
       <section>
         <p>${correctnessHTML}</p>
-        <button>next</button>
+        <button class='startButtonNext'>next</button>
       </section>
     `
 
@@ -76,9 +85,87 @@ function renderFeedback(isCorrect, currentAns) {
   $('.inner-quiz-box').html(feedbackHTML)
 }
 
+function continueFeedback() {
+  $('.quiz').on('click', '.startButtonNext', function (event) {
+    renderAQuestion(questionNumber);
+    // alert(questionNumber);
+  })
+}
+
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////final pages/////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+function determineFinalCorrectness(selectedOption, item, questionNumber) {
+  let currentAns = item.answer;
+  // console.log(currentAns);
+  // console.log(selectedOption);
+
+  const isCorrect = currentAns === selectedOption ? true : false;
+  // console.log(isCorrect);
+  renderFinalFeedback(isCorrect, currentAns, questionNumber);
+}
+
+function renderFinalFeedback(isCorrect, currentAns, questionNumber) {
+  // HTML portion of feedback reliant on correctness
+  if (isCorrect) {
+    console.log('inside final rederfeedback');
+    $(`li #${questionNumber}`).removeClass('far fa-square').addClass('far fa-plus-square');
+    // console.log(imageCont);
+    correctnessHTML = `Correct!`;
+    score += 1;
+  } else {
+    // alert('esNawt')
+    $(`li #${questionNumber}`).removeClass('far fa-square').addClass('far fa-minus-square');
+    correctnessHTML = `Inccorrect, correct answer is ${currentAns}`
+  }
+
+  // incorporate that into the general HTML feedback 
+  const feedbackHTML = `
+      <section>
+        <p>${correctnessHTML}</p>
+        <button class='finalNext'>next</button>
+      </section>
+    `
+
+  // render the HTML
+  $('.inner-quiz-box').html(feedbackHTML)
+}
+
+function continueFinalFeedback() {
+  $('.quiz').on('click', '.finalNext', function (event) {
+    finalPage();
+    // alert(questionNumber);
+  })
+}
+
+function finalPage() {
+  perc = (score/10)*100
+  const questionHtml = $(`
+      <h3>Thanks for testing you geog skillz!</h3>
+      <p>Your score is ${score}/10!</p>
+      <p>That's ${perc}%!</p>
+      <button type="button" class="startOverButton button">start over</button>
+  `);
+
+  $(".inner-quiz-box").html(questionHtml);
+}
+
+function returnToBeginning() {
+  $('.quiz').on('click', '.startOverButton', function (event) {
+    questionNumber = 0;
+    score = 0;
+    renderAQuestion(questionNumber);
+    // alert(questionNumber);
+  })
+}
+
 function handleQuizApp() {
   getStarted();
   nextQuestion();
+  continueFeedback();
+  continueFinalFeedback();
+  returnToBeginning();
 }
 
 $(handleQuizApp());
